66c273a40c83dce218975c0f1b9461ba83d3bf2b
sakkke <84666033+sakkke@users.noreply.github.com>
2023-12-07 22:41:08 +0900

    feat: add offline installation

---
+++ configs/baseline/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly CWD="$(cd "$(dirname "$0")" && pwd)"
+
+function main {
+  local packages=(
+    amd-ucode
+    base
+    base-devel
+    grub
+    intel-ucode
+    kde-applications
+    linux
+    linux-firmware
+    micro
+    networkmanager
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    noto-fonts-extra
+    plasma
+    sddm
+    sudo
+    xorg-drivers
+    xorg-server
+  )
+
+  local cachedir="$CWD/cachedir"
+  local dbpath="$CWD/dbpath"
+  mkdir -p "$dbpath"
+  pacman --cachedir "$cachedir" --noconfirm -Swyb "$dbpath" "${packages[@]}"
+
+  local pkgs="$CWD/airootfs/.pkgs"
+  cp -r "$cachedir" "$pkgs"
+
+  local db="$pkgs/live.db.tar.gz"
+  find "$pkgs" -regex '.*\(\.pkg\.tar\.xz\|\.pkg\.tar\.zst\)$' | xargs repo-add "$db"
+
+  mkarchiso "$@"
+}
+
+main "$@"
